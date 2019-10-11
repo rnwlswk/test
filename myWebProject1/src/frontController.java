c490d3fddb8b0e7496567362b4b92a786beaf041
yj-605-05 <yj-605-05@DESKTOP-679AQCT>
2019-10-11 14:24:55 +0900

    test

---
+++ myWebProject1/src/frontController.java
@@ -0,0 +1,30 @@
+
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/front")
+public class frontController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+		RequestDispatcher rd = request.getRequestDispatcher("/myTest.jsp");
+		rd.forward(request, response);
+				
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ myWebProject1/src/myWebProject1/ReadText1.java
@@ -0,0 +1,28 @@
+package myWebProject1;
+ 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+ 
+public class ReadText1 {
+    public static void main(String[] args){
+        try{
+            //파일 객체 생성
+            File file = new File("C:/sample.txt");
+            //입력 스트림 생성
+            FileReader filereader = new FileReader(file);
+            int singleCh = 0;
+            while((singleCh = filereader.read()) != -1){
+                System.out.print((char)singleCh);
+            }
+            filereader.close();
+        }catch (FileNotFoundException e) {
+            // TODO: handle exception
+        }catch(IOException e){
+            System.out.println(e);
+        }
+    }
+}
+
+
